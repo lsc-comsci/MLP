18edf7e9c786833d8e5b0378e44a1c40654dff52
Amr Ojjeh <amrojjeh@outlook.com>
2022-10-25 21:03:22 -0500

    Comply with PyLint

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
+"""
+The main python file. Write all your scenes here!
+"""
+
 from basics import in_dev, scene, play
 
+# The @scene tells the code that this is a new scene
+# The ctx stores the game data.
 @scene
 def name_scene(ctx):
-	name = input("Enter your name: ")
-	ctx.name = name
-	yn = input("Would you like to be a kettle? ").lower()
-	if yn == 'y':
-		return yes_kettle_scene
-	else:
-		return no_kettle_scene
+    """
+This function stores the name inside ctx so that it can be used by later.
+    """
+    name = input("Enter your name: ")
+    ctx = name
+    yes_no = input("Would you like to be a kettle? ").lower()
+    if yes_no == 'y':
+        return yes_kettle_scene
+    return no_kettle_scene
 
 @scene
 def yes_kettle_scene(ctx):
-	in_dev()
+    """
+Please contribute to this scene!
+    """
+    in_dev()
 
 @scene
 def no_kettle_scene(ctx):
-	in_dev()
+    """
+Please contribute to this scene!
+    """
+    in_dev()
 
 play(name_scene)
