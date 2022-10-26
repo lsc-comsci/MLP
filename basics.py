36a3785c9b5e4a16c978b11e53cac4929b831378
Amr Ojjeh <amrojjeh@outlook.com>
2022-10-26 12:11:09 -0500

    Simplify code

--- basics.py
+++ basics.py
@@ -3,8 +3,6 @@ Don't update this code without telling the officers!
 The minimalist framework to manage the different scenes
 """
 
-import functools
-
 def in_dev():
     """
 In development! Please contribute to scenes that are in development.
@@ -17,28 +15,13 @@ The function is called once the game runs out of scenes
     """
     print("===THE GAME HAS ENDED===")
 
-def scene(func):
-    """
-Decorator for scene functions.
-Stores the function in played_scenes when ran
-Also assigns the "scene" attr to the function
-    """
-    @functools.wraps(func)
-    def wrapper_scene(ctx):
-        if "played_scenes" in ctx:
-            ctx["played_scenes"].append(func.__name__)
-        else:
-            ctx["played_scenes"] = [func.__name__]
-        return func(ctx)
-    wrapper_scene.scene = True
-    return wrapper_scene
-
 def play(curr_scene):
     """
 Starts the game!
     """
-    ctx = {}
-    while hasattr(curr_scene, "scene"):
+    ctx = {"played_scenes": []}
+    while curr_scene:
+        ctx["played_scenes"].append(curr_scene.__name__)
         curr_scene = curr_scene(ctx)
 
     ended()
--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 The main python file. Write all your scenes here!
 """
 
-from basics import in_dev, scene, play
+from basics import in_dev, play
 
 # The @scene tells the code that this is a new scene
 # The ctx stores the game data.
-@scene
 def name_scene(ctx):
     """
-This function stores the name inside ctx so that it can be used by later.
+Amr Ojjeh (amrojjeh)
+ctx[name] stores the name of the player
     """
     name = input("Enter your name: ")
     ctx["name"] = name
@@ -18,14 +18,12 @@ This function stores the name inside ctx so that it can be used by later.
         return yes_kettle_scene
     return no_kettle_scene
 
-@scene
 def yes_kettle_scene(ctx):
     """
 Please contribute to this scene!
     """
     in_dev()
 
-@scene
 def no_kettle_scene(ctx):
     """
 Please contribute to this scene!
