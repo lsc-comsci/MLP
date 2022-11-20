bd2d40f0653cb9930393608d8116665552bf0318
julio amaya <julioamaya127@gmail.com>
2022-11-20 12:57:30 -0600

    made some changes and added new scenes.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ The main python file. Write all your scenes here!
 
 from basics import in_dev, play
 
+#lists for use in functions when needed
+answer_yes = ['Yes', 'Y', 'yes', 'y']
+answer_no = ['No', 'N', 'no', 'n']
+
 # The ctx stores the game data.
 def name_scene(ctx):
     """
@@ -13,9 +17,11 @@ ctx[name] stores the name of the player
     name = input("Enter your name: ")
     ctx["name"] = name
     yes_no = input("Would you like to be a kettle? ").lower()
-    if yes_no == 'y':
+    if yes_no in answer_yes:
         return yes_kettle_scene
     return no_kettle_scene
+
+
 
 def yes_kettle_scene(ctx):
     """
@@ -26,8 +32,8 @@ Ricardo Bernabe (BallinMonkey12)
     print("You wake up and go to the restroom")
     print('Look in the mirror? Yes / No')
 
-    answer_yes = ['Yes', 'Y', 'yes', 'y']
-    answer_no = ['No', 'N', 'no', 'n']
+   # answer_yes = ['Yes', 'Y', 'yes', 'y']
+   # answer_no = ['No', 'N', 'no', 'n']
 
     ans1= input(">>")
 
@@ -39,7 +45,8 @@ Ricardo Bernabe (BallinMonkey12)
         ans2= input(">>")
 
         if ans2 in answer_yes:
-            print('You run back to bed and pretend it was a dream')
+            print('You run back to bed and pretend it was a dream...')
+            return back_to_bed
 
         elif ans2 in answer_no:
             print("Something chokes you to death")
@@ -49,8 +56,42 @@ Ricardo Bernabe (BallinMonkey12)
     else:
         return no_look_mirror
 
+def back_to_bed(ctx):
+    print("You realize it wasn't a dream and are in great danger. What do you do?")
+    print("""1) Call the police
+2) Get out of bed and fight it
+3) Stay in the bed until you rust out and die
+4) Pray to god""")
+    anss = int(input(">>> "))
+    match anss:
+        case 1:
+            print("Oops! You have no fingers so there's no way you can call the police!")
+            return back_to_bed
+        case 2:
+            print("You tried your best to fight this dark evil but you were crushed.")
+            print("=== You died ===")
+            goback = input("Go back one scene? ")
+            if goback in answer_yes:
+                return back_to_bed
+        case 3:
+            print("You have successfully rusted out.")
+            print("=== You died ===")
+            goback = input("Go back one scene? ")
+            if goback in answer_yes:
+                return back_to_bed
+        case 4:
+            print("""Kettle Jesus showed up from the heavens through your roof, you can barely see him! he is too bright!
+He saved you. He destroyed the dark evil that was hunting you.""")
+            return jesus_saved_you
+
+def jesus_saved_you(ctx):
+    in_dev()
+
+
 def no_look_mirror(ctx):
     in_dev()
+
+
 
 def no_kettle_scene(ctx):
     """
@@ -63,7 +104,7 @@ ctx["age"] is the age of the chicken days
     ctx["hunger"] = 0
     ctx["age"] = 0
     print("Since you're not a kettle, you're a chicken.")
-    yes_no = input("Do you eat grain?")
+    yes_no = input("Do you eat grain? ")
     if "y" in yes_no:
         return eats_grain_scene
     return not_normal_chicken_scene
@@ -79,7 +120,7 @@ def not_normal_chicken_scene(ctx):
     """
 Written during the club
     """
-    print("Now you must eat milk. What do you do?")
+    print("Now you must eat milk, but how are you going to turn the milk to solid state?. What do you do?")
     print("""1) Wait until night
 2) Call the butcher
 3) Wait for the milk to expire
@@ -98,7 +139,8 @@ Written during the club
             return dark_magic_scene
 
 def butcher_scene(ctx):
-    in_dev()
+    print("The butcher was hungry and ate you.")
+    print('=== You died ===')
 
 def dark_magic_scene(ctx):
     in_dev()
